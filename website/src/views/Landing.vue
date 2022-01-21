14bea7cf3590a77bf60607ee8a76bf6eb3cee795
Andrea <andrea.9940@gmail.com>
2022-01-21 10:58:30 +0000

    Localize landing page

--- website/src/views/Landing.vue
+++ website/src/views/Landing.vue
@@ -7,7 +7,7 @@ Landing page for a quick intro of MapMyMountains.
 	<b-row align-v="center" style="min-height:calc(100vh - 12rem)">
 		<b-col xl="3" class="text-center">
 			<h1>Map My Mountains</h1>
-  			<p>Aiuta a costruire un database di montagne libero, per tutti.</p>
+  			<p>{{localization.landing_help_build}}</p>
 		</b-col>
 		<b-col xl="9">
 			<img src="../assets/screenshot.jpg" style="width:100%" class="shadow">
@@ -15,30 +15,30 @@ Landing page for a quick intro of MapMyMountains.
 	</b-row>
 	<b-row align-v="center" style="height:8rem">
 		<b-col>
-			<h2 class="text-center">Come funziona?</h2>
+			<h2 class="text-center">{{localization.landing_how_it_works}}</h2>
 		</b-col>
 	</b-row>
 	<b-row style="margin-top:10rem">
 		<b-col>
 			<b-row align-v="center" style="height:4rem">
 				<b-col>
-					<h5 class="text-center">Esplora le aree che conosci.</h5>
+					<h5 class="text-center">{{localization.landing_explore}}</h5>
 				</b-col>
 			</b-row>
 			<b-row>
 				<b-col md="4">
-					<b-card class="my-3" title="Dolomiti">
-						<p class="card-text">Le Dolomiti, anche dette Monti pallidi (Dolomiten in tedesco, Dolomites in ladino, Dolomitis in friulano), sono un insieme di gruppi montuosi delle Alpi Orientali italiane, a sud della catena principale alpina, comprese tra Veneto, Trentino-Alto Adige e Friuli, tra le province di Belluno, Bolzano, Trento, Udine e Pordenone.</p>
+					<b-card class="my-3" :title="localization.landing_dolomites">
+						<p class="card-text">{{localization.landing_dolomites_description}}</p>
 					</b-card>
 				</b-col>
 				<b-col md="4">
-					<b-card class="my-3" title="Alpi Orobie">
-						<p class="card-text">Le Alpi Orobie (o, più semplicemente Orobie) sono una sottosezione delle Alpi e Prealpi Bergamasche. Limitate a nord dalla sponda meridionale della Valtellina inferiore che dal passo dell'Aprica si spinge in direzione ovest sino all'insenatura di Piona, all'estremità settentrionale del lago di Como.</p>
+					<b-card class="my-3" :title="localization.landing_bergamasque_alps">
+						<p class="card-text">{{localization.landing_bergamasque_alps_description}}</p>
 					</b-card>
 				</b-col>
 				<b-col md="4">
-					<b-card class="my-3" title="Triangolo Lariano">
-						<p class="card-text">Il Triangolo Lariano o Penisola Lariana è la parte di terra compresa fra i due rami del Lago di Como, detto anche "Lario". Occupato da rilievi montuosi prealpini, che culminano col Monte San Primo (1.686 metri), è tagliato in senso verticale dal solco della Valassina (o Vallassina), entro cui scorre il primo tratto del fiume Lambro.</p>
+					<b-card class="my-3" :title="localization.landing_triangolo_lariano">
+						<p class="card-text">{{localization.landing_triangolo_lariano_description}}</p>
 					</b-card>
 				</b-col>
 			</b-row>
@@ -48,7 +48,7 @@ Landing page for a quick intro of MapMyMountains.
 		<b-col>
 			<b-row align-v="center" style="height:4rem">
 				<b-col>
-					<h5 class="text-center">Trova le montagne che ami.</h5>
+					<h5 class="text-center">{{localization.landing_find_mountains}}</h5>
 				</b-col>
 			</b-row>
 			<b-row>
@@ -69,7 +69,7 @@ Landing page for a quick intro of MapMyMountains.
 		<b-col>
 			<b-row align-v="center" style="height:4rem">
 				<b-col class="text-center">
-					<h5>Mappa le montagne!</h5>
+					<h5>{{localization.landing_map}}</h5>
 				</b-col>
 			</b-row>
 			<b-row>
@@ -82,7 +82,7 @@ Landing page for a quick intro of MapMyMountains.
 	<b-row>
 		<b-col class="text-center h4" style="margin-top:10rem;margin-bottom:10rem">
 			<b-link to="/register">
-				Registrati e inizia a mappare!
+				{{localization.landing_register}}
 			</b-link>
 		</b-col>
 	</b-row>
@@ -90,12 +90,15 @@ Landing page for a quick intro of MapMyMountains.
 </template>
 
 <script>
+import localization from '@/assets/localization';
+
 export default {
 	/**
 	 * Data object for the Vue instance.
 	 */
 	data() {
 		return {
+			localization
 		}
 	},
 }
